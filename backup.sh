0a3aa64680418cd2c405aebf8f605d5264fae4ab
Max Dobeck <msdobeck@gmail.com>
2019-04-13 16:38:55 -0700

    Added Backgrounds dir and ovpns dir. Preparing to create hard drive backup on pi.

--- backup.sh
+++ backup.sh
@@ -9,5 +9,5 @@ cp ~/.bash_aliases ~/Backup
 echo "Copied ~/.bash_aliases to ~/Backup"
 
 # Tar it
-tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/backup.tar.gz ~/Backup ~/Documents ~/scripts
+tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/backup.tar.gz ~/Backup ~/ovpns ~/Pictures/Backgrounds  ~/Documents ~/scripts
 tput setaf 1; echo "/tmp/backup.tar.gz created"
