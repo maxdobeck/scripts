16abc2c07fd09108eadf1d19101e15db76240f0e
Max Dobeck <msdobeck@gmail.com>
2019-05-04 13:52:44 -0700

    Added more explicit stdout about scp process.

--- backup.sh
+++ backup.sh
@@ -13,4 +13,5 @@ tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/backup.tar.gz ~/Backup ~/ovpns ~/Pi
 tput setaf 1; echo "/tmp/backup.tar.gz created"
 
 # scp to the media server
+echo "moving /tmp/backup.tar.gz to $LOCALMAIN_SERVER"
 scp /tmp/backup.tar.gz $LOCALMAIN_USER@$LOCALMAIN_SERVER:/home/mdobeck/media/backups
