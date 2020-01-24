b6234a73f1b6b1606f32b79d90ecc2ab0bc4ca44
Max Dobeck <msdobeck@gmail.com>
2020-01-23 20:37:58 -0800

    Added .gitignore and updated the backup script.

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ cp ~/.bash_aliases ~/Backup
 echo "Copied ~/.bash_aliases to ~/Backup"
 
 # Tar it
-tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/backup.tar.gz ~/Backup ~/ovpns ~/Pictures/Backgrounds  ~/Documents ~/scripts
+tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/backup.tar.gz ~/Backup ~/ovpns ~/Pictures/Backgrounds  ~/Documents ~/Music ~/Audio/raw ~/scripts
 tput setaf 1; echo "/tmp/backup.tar.gz created"
 
 # scp to the media server
