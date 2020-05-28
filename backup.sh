96ecf7ecc049a3f62cfeca1b652c7cffddbf19be
Max Dobeck <msdobeck@gmail.com>
2020-05-28 16:51:51 -0700

    updated exclude file and the backup script.  Less verbose.

--- backup.sh
+++ backup.sh
@@ -3,18 +3,21 @@
 # max dobeck
 # github.com/maxdobeck
 
+# example to generate hash of tar directory
+# tar -c ~/Music/ | md5sum | awk '{ print $1}'
+
 # Copy critical stuff before zipping
 cp ~/.bashrc ~/Backup
 echo "Copied ~/.bashrc file to ~/Backup"
 cp ~/.bash_aliases ~/Backup
 echo "Copied ~/.bash_aliases to ~/Backup"
 
-dirs=("Backup" "ovpns" "Pictures/Backgrounds" "Documents" "Music" "Audio/raw" "scripts" "cheatsheet_project")
+dirs=("Backup" "ovpns" "Pictures/Backgrounds" "Documents" "Music" "Audio/raw" "scripts" "cheatsheet_project" "rust")
 
 for dir in "${dirs[@]}"
 do
     fn=$(echo "$dir" | tr / _)
-    GZIP=-9 tar -X ~/scripts/tar-exclude.txt -zcvf  /tmp/$fn.tar.gz ~/$dir
+    GZIP=-9 tar -X ~/scripts/tar-exclude.txt -zcf  /tmp/$fn.tar.gz ~/$dir
     echo /tmp/$dir.tar.gz created
 done
 
@@ -23,7 +26,7 @@ for dir in "${dirs[@]}"
 do
     fn=$(echo "$dir" | tr / _)
     echo "moving /tmp/$fn.tar.gz to $LOCALMAIN_SERVER"
-    scp /tmp/$fn.tar.gz $LOCALMAIN_USER@$LOCALMAIN_SERVER:/home/mdobeck/media/backups
+    scp /tmp/$fn.tar.gz $LOCALMAIN_USER@$LOCALMAIN_SERVER:/home/mdobeck/media/backups &
 done
 
 
